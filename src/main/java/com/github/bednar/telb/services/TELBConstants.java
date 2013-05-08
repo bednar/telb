fbc19a446f8f9979149b604f590e3c6e1d966c87
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-05-08 14:46:24 +0200

    Prefix for Tapestry5 bindings.

---
+++ src/main/java/com/github/bednar/telb/services/TELBConstants.java
@@ -0,0 +1,14 @@
+package com.github.bednar.telb.services;
+
+/**
+ * Constants of 'Tapestry5 Expression Language Binding'.
+ *
+ * @author Jakub Bednář (08/05/2013 2:44 PM)
+ */
+public final class TELBConstants
+{
+    /**
+     * Binding expression prefix used for expression language evaluation.
+     */
+    public static final String EXPRESSION_LANGUAGE_BINDING = "el";
+}

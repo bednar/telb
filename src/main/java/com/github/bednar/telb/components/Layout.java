8db8f07fd910844d605b0adedf6f1f2dd936d0c0
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-05-08 14:43:41 +0200

    Can start as Tapestry5 application.

---
+++ src/main/java/com/github/bednar/telb/components/Layout.java
@@ -0,0 +1,8 @@
+package com.github.bednar.telb.components;
+
+/**
+ * @author Jakub Bednář (08/05/2013 2:34 PM)
+ */
+public class Layout
+{
+}
---
+++ src/main/java/com/github/bednar/telb/pages/Index.java
@@ -0,0 +1,8 @@
+package com.github.bednar.telb.pages;
+
+/**
+ * @author Jakub Bednář (08/05/2013 2:37 PM)
+ */
+public class Index
+{
+}
